d559c188bbc614d5f9c3693c011a2b97e98b2d75
Denis Atkesone <denis.atkesone@gmail.com>
2017-11-07 11:50:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog
 
-  def name=(dog_name)
-    @name = dog_name
+  def name=(new_name)
+    @name = new_name
   end
 
   def name
     @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def breed
+    @breed
   end
 end
